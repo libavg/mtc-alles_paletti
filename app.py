33eb38c59b9fbbe9271dfd1ad37b0e6f0d832b89
mh <mh@libavg.de>
2009-04-30 16:38:49 +0000

    alles_paletti: posterized paint.png, added license
    svn path=/trunk/avg_media/mtc/alles_paletti/; revision=3793

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
-# coding=utf8
+
+# Copyright (C) 2009
+#    Nils Dagsson Moskopp (erlehmann) <nils@dieweltistgarnichtso.net>
+#
+# alles paletti is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# alles paletti is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with alles paletti.  If not, see <http://www.gnu.org/licenses/>.
 
 from libavg import avg, Point2D, Bitmap, anim, AVGApp
 from libavg import AVGMTAppStarter
