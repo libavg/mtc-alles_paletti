769ae5d24ce693b7ee3fb5b23eb12baa14f3233e
mh <mh@libavg.de>
2009-04-30 16:41:07 +0000

    alles_paletti: removed bogus imports
    svn path=/trunk/avg_media/mtc/alles_paletti/; revision=3794

--- __init__.py
+++ __init__.py
@@ -1,10 +1,6 @@
 import os
-from libavg import avg
-from libavg.mathutil import getScaledDim
 from libavg.AVGAppUtil import getMediaDir, createImagePreviewNode
 from . import app
-
-g_player = avg.Player.get()
 
 
 def createPreviewNode(maxSize):
